c9d0c18a6f8d961e7f88c9c23481948b29529a81
mytoh <mytoh@dixneuf.local>
2010-10-12 01:14:01 +0900

    modified:   xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -8,7 +8,7 @@ import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
 import XMonad.Util.Run
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
 import Data.Monoid
 import System.Exit
 import System.IO
@@ -20,23 +20,30 @@ myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 myBorderWidth   = 1
 myModMask       = mod1Mask
-myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+myWorkspaces    =  ["yk", "ka" ,"ko", "ne", "vi", "ku", "se", "kasi", "ysi"] 
 myNormalBorderColor  = "#000000"
 myFocusedBorderColor = "#0066ff"
-myLayout =  smartBorders $ avoidStruts $  tiled ||| Mirror tiled ||| Full
+
+-- myKeys = \c -> mkKeymap c $
+--                 [ ("M-p", spawn "exe=`dmenu_path | dmenu -l 7 -nb black -nf darkgrey -fn 'terminus-8' ` && eval \"exec $exe\"") ]
+
+myLayout =  smartBorders $ avoidStruts $  (tiled ||| Mirror tiled ||| Full)
   where
      tiled   = Tall nmaster delta ratio
      nmaster = 1
      ratio   = 1/2
      delta   = 3/100
+     
 myManageHook = composeAll
     [ isFullscreen                  --> (doF W.focusDown <+> doFullFloat),
       className =? "MPlayer"        --> doFloat,
       className =? "Gimp"           --> doFloat,
       className =? "Thunar"         --> doFloat,
+      className =? "Main.py"        --> doFloat,
       resource  =? "desktop_window" --> doIgnore,
       resource  =? "kdesktop"       --> doIgnore 
       ] <+> manageDocks <+> manageHook defaultConfig 
+      
 myLogHook h = dynamicLogWithPP $ defaultPP { 
                 ppCurrent         = dzenColor "#303030" "#909090" . pad,
                 ppHidden          = dzenColor "#909090" "" .pad,
@@ -48,13 +55,15 @@ myLogHook h = dynamicLogWithPP $ defaultPP {
                 ppSep             = "  ",
                 ppOutput          = hPutStrLn h
                 }
-myDzenBar = "dzen2 -p -ta l  -x 0 -y 0 -w 400 -h 12 -fn '-mplus-sys-medium-r-normal--10-*' -fg '#606060' -bg '#303030' -e 'onexit=ungrabmouse'"
-myConkyBar  = "conky -c ~/.conkyrc | dzen2 -p -ta r -x 400 -y 0 -w 880 -h 12 -fn '-mplus-sys-medium-r-normal--10-*' -fg '#606060' -bg '#303030' -e 'onexit=ungrabmouse'"
+myDzenBar = "dzen2 -p -ta l  -x 0 -y 0 -w 400 -h 12 -fn '-mplus-hlv-medium-r-normal--10-*' -e 'onexit=ungrabmouse'"
+myConkyBar  = "conky -c ~/.conkyrc | dzen2 -p -ta r -x 400 -y 0 -w 880 -h 12 -fn '-mplus-hlv-medium-r-normal--10-*' -e 'onexit=ungrabmouse'"
 
-myEventHook = mempty
-myStartupHook = return ()
+myEventHook = ewmhDesktopsEventHook
 
-main = do 
+myStartupHook = return () 
+
+main = myConfig
+myConfig = do
       d <- spawnPipe myDzenBar
       spawn myConkyBar
       xmonad $ ewmh $ withUrgencyHook NoUrgencyHook $ defaultConfig {
@@ -62,6 +71,7 @@ main = do
         focusFollowsMouse  = myFocusFollowsMouse,
         borderWidth        = myBorderWidth,
         modMask            = myModMask,
+--      keys               = myKeys,
         workspaces         = myWorkspaces,
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
