3b48d77720a3f23afbd34c3ba0c941b5485fbdc5
mytoh <mytoh@dixneuf.local>
2010-10-11 20:25:46 +0900

    some changes

--- xmonad.hs
+++ xmonad.hs
@@ -48,8 +48,8 @@ myLogHook h = dynamicLogWithPP $ defaultPP {
                 ppSep             = "  ",
                 ppOutput          = hPutStrLn h
                 }
-myDzenBar = "dzen2 -p -ta l  -x 0 -y 0 -w 400 -h 16 -fg '#606060' -bg '#303030' -e 'onexit=ungrabmouse'"
-myConkyBar  = "conky -c ~/.conkyrc | dzen2 -p -ta r -x 400 -y 0 -w 880 -h 16  -fg '#606060' -bg '#303030' -e 'onexit=ungrabmouse'"
+myDzenBar = "dzen2 -p -ta l  -x 0 -y 0 -w 400 -h 12 -fn '-mplus-sys-medium-r-normal--10-*' -fg '#606060' -bg '#303030' -e 'onexit=ungrabmouse'"
+myConkyBar  = "conky -c ~/.conkyrc | dzen2 -p -ta r -x 400 -y 0 -w 880 -h 12 -fn '-mplus-sys-medium-r-normal--10-*' -fg '#606060' -bg '#303030' -e 'onexit=ungrabmouse'"
 
 myEventHook = mempty
 myStartupHook = return ()
