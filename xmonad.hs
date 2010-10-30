5e20ef1b10e7abdc01b3be02b3cc4b99ab7e69a6
mytoh <mytoh@gmail.com>
2010-10-31 02:45:25 +0900

    add some key bindings

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,14 @@
 
-import XMonad
+import XMonad hiding ( (|||) )
 import XMonad.ManageHook
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+import Data.Monoid
+import System.Exit
+import System.IO
+
+import XMonad.Actions.CycleWS
 
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -10,6 +18,7 @@ import XMonad.Hooks.EwmhDesktops
 
 import XMonad.Layout.NoBorders
 import XMonad.Layout.DecorationMadness
+import XMonad.Layout.LayoutCombinators
 
 import XMonad.Prompt
 import XMonad.Prompt.Shell
@@ -17,12 +26,8 @@ import XMonad.Prompt.Shell
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
 import XMonad.Util.Themes
-
-import Data.Monoid
-import System.Exit
-import System.IO
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
+import XMonad.Util.Scratchpad
+import XMonad.Util.WorkspaceCompare
 
 myTerminal    = "urxvtc "
 myFocusFollowsMouse :: Bool
@@ -33,100 +38,19 @@ myWorkspaces    =  ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 myNormalBorderColor  = "#000000"
 myFocusedBorderColor = "#0066ff"
 
---------------------------------------------------------
+-- Layouts
+myLayout = avoidStruts $ smartBorders $ (tallTabbed shrinkText (theme smallClean) ||| Full)
+
 -- keybindings
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+myKeys = [
+     ("M-p", shellPrompt myXPConfig),
+     ("M-f", sendMessage $ JumpToLayout "Full"),
+     ("M-<R>", moveTo Next (WSIs notSP)),
+     ("M-<L>" , moveTo Prev (WSIs notSP)) 
+     ]
+     where
+        notSP = (return $ ("SP" /=) . W.tag) :: X (WindowSpace -> Bool)
 
-    -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-
-    -- launch dmenu
- -- , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu -fa Inconsolata -nb grey -nf white` && eval \"exec $exe\"")
-    , ((modm,               xK_p     ), shellPrompt myXPConfig)
-
-    -- launch gmrun
-    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
-
-    -- close focused window
-    , ((modm .|. shiftMask, xK_c     ), kill)
-
-     -- Rotate through the available layout algorithms
-    , ((modm,               xK_space ), sendMessage NextLayout)
-
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
-    -- Resize viewed windows to the correct size
-    , ((modm,               xK_n     ), refresh)
-
-    -- Move focus to the next window
-    , ((modm,               xK_Tab   ), windows W.focusDown)
-
-    -- Move focus to the next window
-    , ((modm,               xK_j     ), windows W.focusDown)
-
-    -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
-
-    -- Move focus to the master window
-    , ((modm,               xK_m     ), windows W.focusMaster  )
-
-    -- Swap the focused window and the master window
-    , ((modm,               xK_Return), windows W.swapMaster)
-
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-
-    -- Shrink the master area
-    , ((modm,               xK_h     ), sendMessage Shrink)
-
-    -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
-
-    -- Push window back into tiling
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-
-    -- Increment the number of windows in the master area
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-
-    -- Deincrement the number of windows in the master area
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-
-    -- Quit xmonad
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-
-    -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-    ]
-    ++
-
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
-
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-
-----------------------------------------------------
 -- prompt configuration
 myXPConfig = defaultXPConfig {
               position        = Bottom,
@@ -140,16 +64,16 @@ myXPConfig = defaultXPConfig {
               }
 
 
--- Layouts
-myLayout = avoidStruts $ smartBorders $ (tallTabbed shrinkText (theme smallClean) ||| Full)
      
 myManageHook = composeAll
     [ isFullscreen                  --> (doF W.focusDown <+> doFullFloat),
+      isDialog                      --> doCenterFloat,
       className =? "MPlayer"        --> doFloat,
-      className =? "Gimp"           --> doFloat,
-      className =? "Thunar"         --> doFloat,
       className =? "Main.py"        --> doFloat,
-      className =? "DTA"        --> doFloat,
+      className =? "Gimp"           --> doFloat,
+      className =? "DTA"            --> doFloat,
+      className =? "Firefox" <&&> resource /=? "Navigator"  --> doFloat,
+      className =? "Thunar"         --> doCenterFloat,
       resource  =? "desktop_window" --> doIgnore
       ] <+> manageDocks <+> manageHook defaultConfig 
       
@@ -182,7 +106,6 @@ myConfig = do
         focusFollowsMouse  = myFocusFollowsMouse,
         borderWidth        = myBorderWidth,
         modMask            = myModMask,
-        keys               = myKeys,
         workspaces         = myWorkspaces,
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
@@ -191,4 +114,4 @@ myConfig = do
         handleEventHook    = myEventHook,
         logHook            = myLogHook d,
         startupHook        = myStartupHook
-    }
+    } `additionalKeysP` myKeys
