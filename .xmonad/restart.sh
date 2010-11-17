44742eda9e65bcd970382a45851e89e42fb90956
mytoh <mytoh@gmail.com>
2010-11-17 20:20:55 +0900

    change to use tcsh

--- .xmonad/restart.sh
+++ .xmonad/restart.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/csh
+
+killall dzen2
+killall trayer
+killall gmail-notifier
+xmonad --recompile 
+xmonad --restart
 
 # for pid in `pgrep dzen2`; do kill -9 $pid; done; killall trayer; killall gmail-notifier; xmonad --recompile && xmonad --restart
-killall dzen2; killall trayer; killall gmail-notifier; xmonad --recompile && xmonad --restart
