3f6ac4238e641d0de138ed79a811b1875f744aec
mytoh <mytoh@gmail.com>
2010-12-23 01:16:11 +0900

    add scsh script file

---
+++ .scsh/ln.scm
@@ -0,0 +1,17 @@
+#!/usr/local/bin/scsh -s
+!#
+
+(define gitdir "/home/mytoh/local/git/dotfiles/")
+
+(define dotfiles (list ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
+
+(define (link f)
+  (if (null? f)
+        (display "script finished!")
+      (begin
+        (create-symlink (string-append gitdir (car f)) (string-append home-directory "/" (car f)) 'delete)
+        (link (cdr f)))))
+
+(link dotfiles)
+
+
