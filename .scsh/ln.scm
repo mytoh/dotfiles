97ce20a69a849acf7fa2f5fe16b83629eb0a120f
mytoh <mytoh@gmail.com>
2010-12-23 22:24:26 +0900

    change definition

--- .scsh/ln.scm
+++ .scsh/ln.scm
@@ -1,18 +1,16 @@
 #!/usr/local/bin/scsh -s
 !#
 
-(define gitdir "/home/mytoh/local/git/dotfiles/")
+(define gitdir (string-append home-directory "/" "git/dotfiles/")
 
 (define dotfiles (list ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
 
-(define (link f)
-  (let loop ((f f))
+(let loop ((f dotfiles))
   (if (null? f)
         (display "script finished!")
       (begin
-        (create-symlink (string-append gitdir (car f)) (string-append home-directory "/" (car f))) 
-        (loop (cdr f))))))
+        (create-symlink (string-append gitdir (car f)) (string-append home-directory "/" (car f)) 'delete) 
+        (loop (cdr f)))))
 
-(link dotfiles)
 
 
