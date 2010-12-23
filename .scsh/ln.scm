bc41087d8cc007d37d0055f93deeecf77b4c8730
mytoh <mytoh@gmail.com>
2010-12-23 22:02:18 +0900

    change to use let

--- .scsh/ln.scm
+++ .scsh/ln.scm
@@ -6,11 +6,12 @@
 (define dotfiles (list ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
 
 (define (link f)
+  (let loop ((f f))
   (if (null? f)
         (display "script finished!")
       (begin
-        (create-symlink (string-append gitdir (car f)) (string-append home-directory "/" (car f)) 'delete)
-        (link (cdr f)))))
+        (create-symlink (string-append gitdir (car f)) (string-append home-directory "/" (car f))) 
+        (loop (cdr f))))))
 
 (link dotfiles)
 
