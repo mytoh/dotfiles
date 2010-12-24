1ebdf21f51fca7fd8c9ed59c466c0fb74d16aa14
mytoh <mytoh@gmail.com>
2010-12-25 04:22:55 +0900

    some change

--- .scsh/ln.scm
+++ .scsh/ln.scm
@@ -1,7 +1,7 @@
 #!/usr/local/bin/scsh -s
 !#
 
-(define gitdir (string-append home-directory "/" "git/dotfiles/"))
+(define gitdir (string-append home-directory "/" "local/git/dotfiles/"))
 
 (define dotfiles (list ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
 
