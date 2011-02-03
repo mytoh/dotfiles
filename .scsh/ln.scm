c3a4a47e22d236ab8896d52cf6d02afc4b6a3811
mytoh <mytoh@dixneuf.local>
2011-02-04 07:24:28 +0900

    add linked message

--- .scsh/ln.scm
+++ .scsh/ln.scm
@@ -3,13 +3,15 @@
 
 (define gitdir (string-append home-directory "/" "local/git/dotfiles/"))
 
-(define dotfiles (list ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
+(define dotfiles (list ".emacs" ".emacs-w3m" ".twmrc" ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
 
 (let loop ((f dotfiles))
   (if (null? f)
         (display "script finished!")
       (begin
         (create-symlink (string-append gitdir (car f)) (string-append home-directory "/" (car f)) 'delete) 
+	(display (string-append "linked " (car f)))
+	(newline)
         (loop (cdr f)))))
 
 
