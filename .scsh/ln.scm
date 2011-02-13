ace3e757d6ab884efb61bd7ae932a68b7dd664f7
mytoh <mytoh@gmail.com>
2011-02-14 03:54:23 +0900

    add some files

--- .scsh/ln.scm
+++ .scsh/ln.scm
@@ -3,14 +3,16 @@
 
 (define gitdir (string-append home-directory "/" "local/git/dotfiles/"))
 
-(define dotfiles (list ".emacs" ".emacs-w3m" ".twmrc" ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
+(define dotfiles (list ".emacs" ".emacs-w3m" ".complete.tcsh" ".twmrc" ".cshrc" ".vim" ".gtkrc-2.0" ".screenrc" ".xinitrc" ".stumpwmrc" ".vimrc" ".vimperatorrc" ".sawfishrc" ".Xresources"))
 
 (let loop ((f dotfiles))
   (if (null? f)
+	(begin
         (display "script finished!")
+	(newline))
       (begin
         (create-symlink (string-append gitdir (car f)) (string-append home-directory "/" (car f)) 'delete) 
-	(display (string-append "linked " (car f)))
+	(display (string-append "symlinking " (car f)))
 	(newline)
         (loop (cdr f)))))
 
