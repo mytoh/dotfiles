700458ddbd3894d7dc768ead2e151059615f4ec5
mytoh <mytoh@gmail.com>
2011-11-27 01:35:03 +0900

    4ch-get.scm: added no-clobber option for wget

--- .gosh/4ch-get.scm
+++ .gosh/4ch-get.scm
@@ -1,54 +1,57 @@
 #!/usr/bin/env gosh
 
-(use rfc.http)
-(use rfc.uri)
-(use gauche.process)
+  (use rfc.http)
+  (use rfc.uri)
+  (use gauche.process)
 (use file.util)
 
 
 (define (parse-img line)
  (rxmatch->string #/http\:\/\/images\.4chan\.org\/[^"]+/ line) 
-  )
+ )
 
 ;; "
 
-(define (fetch match)
-  (if (string? match)
-      (run-process `(wget ,match) :wait #t)) 
+  (define (fetch match)
+   (if (string? match)
+    (run-process `(wget -nc ,match) :wait #t)) 
   )
 
 (define (get-img str)
-  (call-with-input-string str
-               (lambda (in) 
-                 (port-for-each
-                   (lambda (line)
-                     (let ((match (parse-img line)))
-                          (fetch match)
-                          )
-                     )
-                   (cut read-line in)
-                   )))
-  )
+ (call-with-input-string str
+  (lambda (in) 
+   (port-for-each
+    (lambda (line)
+     (let ((match (parse-img line)))
+      (fetch match)
+     )
+    )
+    (cut read-line in)
+   )))
+ )
 
 (define (mkdir dir)
+ (if (not (file-exists? dir))
   (make-directory* dir)
-  )
+ )
+)
 
-(define (cd-up)
-  (current-directory "..")
-  )
+(define (cd dir)
+ (if (file-is-directory? dir)
+  (current-directory dir)
+ )
+)
 
 
 (define (main args)
-  (let* ((board (cadr args))
-         (thread (caddr args))
-         (html (values-ref (http-get  "boards.4chan.org"  (string-append "/" board "/res/" thread)) 2))
-         )
-        (begin
-          (mkdir thread)
-          (current-directory thread)
-         (get-img html)
-         (cd-up)
-         )
-         )
-        )
+ (let* ((board (cadr args))
+        (thread (caddr args))
+        (html (values-ref (http-get  "boards.4chan.org"  (string-append "/" board "/res/" thread)) 2))
+       )
+  (begin
+   (mkdir thread)
+   (cd thread)
+   (get-img html)
+   (cd "..")
+  ))
+)
