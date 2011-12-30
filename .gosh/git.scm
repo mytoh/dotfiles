7dded2b75eb94ff7ee9c29ea7b4295be1427844c
mytoh <mytoh@gmail.com>
2011-12-30 10:16:45 +0900

    git.scm: added colour

--- .gosh/git.scm
+++ .gosh/git.scm
@@ -5,16 +5,21 @@
 
 (define-constant gitdir  "~/local/git/")
 
+(define (make-colour colour str)
+    (string-append "[38;5;" (x->string colour) "m" str "[0m"))
+
 (define (update-gitdir gitdir)
   (let ((dirs (list (directory-list (expand-path gitdir) :children? #t :add-path? #t))))
        (let loop ((dirs (car dirs)))
             (if (null? dirs)
                 (display "update finished!\n")
                 (begin
-                  (display "=> ")
-                  (display (car dirs) )
+                  (display (make-colour 4 "=> "))
+                  (display (string-append (sys-dirname (car dirs)) "/" (make-colour 3 (sys-basename (car dirs)))))
                   (newline)
+                  (if (file-is-directory? (car dirs))
                   (run-process '(git pull) :wait #t :directory (car dirs))
+                  #t)
                   (newline)
                   (loop (cdr dirs)))))))
 
