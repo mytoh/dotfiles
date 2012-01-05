5091cf9acbb96a98091c0f234d773ef0505c3695
mytoh <mytoh@gmail.com>
2012-01-05 12:17:09 +0900

    vimproc-post-update: added vimproc update script for git hook

---
+++ .gosh/vimproc-post-update.scm
@@ -0,0 +1,21 @@
+#!/usr/bin/env gosh
+
+#;"original script -> gist.github.com/1261116"
+
+(use gauche.process)
+(use file.util)
+(load "util")
+
+(let* ((hook-dir (current-directory)) #;"git_hook directory"
+       (proc-dir (resolve-path (build-path hook-dir 'up 'up))) #;"vimproc directory"
+       (proc-make (lambda (file)
+                    (current-directory proc-dir)
+                      (run-process `(make -f ,file clean all) :wait #t))))
+       (case (get-os-type)
+         ((darwin)  (proc-make "make_mac.mak"))
+         ((freebsd) (proc-make "make_gcc.mak"))
+         ((linux)   (proc-make "make_gcc.mak"))
+         (else (print "this os is not supported"))))
+  
+  
+  
