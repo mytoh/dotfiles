8cc59d39d60cdad5cd6720cef9f0282193f553cf
mytoh <mytoh@gmail.com>
2012-02-10 06:09:08 +0900

    added build script by gosh

---
+++ .gosh/build/fish-git.scm
@@ -0,0 +1,26 @@
+#!/usr/bin/env gosh
+
+(use gauche.process)
+(use file.util)
+(use kirjasto)
+
+(define *package* "fishfish")
+(define *gitdir* (build-path (home-directory) "local/git"))
+(define *srcdir* (build-path *gitdir* *package*))
+(define *stow-directory* (build-path (home-directory) "local/stow"))
+(define *prefix-directory* (build-path *stow-directory* *package*))
+
+(define (build)
+  (run-process `(./configure ,(string-append "--prefix=" *prefix-directory*)) :wait #t)
+  (run-process '(make clean) :wait #t)
+  (run-process '(make) :wait #t)
+  (run-process '(make install) :wait #t))
+
+(define (stow-install)
+  (cd *stow-directory*)
+  (run-process `(stow -v ,*package*)))
+
+(define (main args)
+  (cd *srcdir*)
+  (build)
+  (stow-install))
