de268252f2116728274472a1a9f77d13e77fb9b7
mytoh <mytoh@gmail.com>
2012-10-03 22:40:42 +0900

    fix gauche-config path

--- .bash.d/gauche.bash
+++ .bash.d/gauche.bash
@@ -2,7 +2,7 @@
 
 
 # gauche {{{
-export GAUCHE_ARCH="$(/usr/local/bin/gauche-config --arch )"
+export GAUCHE_ARCH="$(gauche-config --arch )"
 export GAUCHE_LOAD_PATH="$HOME/.gosh/skripti:$HOME/.gosh:$HOME/.gosh/kirjasto:$HOME/local/share/gauche-0.9/site/lib:$HOME/local/lib/gauche-0.9/site/$GAUCHE_ARCH"
 
 # compdef
