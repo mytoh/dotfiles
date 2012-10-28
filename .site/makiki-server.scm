9be98c7dfad2973c4bc8907df4392ad340798948
mytoh <mytoh@gmail.com>
2012-10-28 13:09:35 +0900

    update

--- .site/makiki-server.scm
+++ .site/makiki-server.scm
@@ -3,7 +3,6 @@
 
 (use gauche.threads)
 (use text.html-lite)
-(use srfi-1)
 (use www.cgi)
 (use makiki)
 (use sxml.tools)
@@ -13,13 +12,13 @@
 (use file.util)
 (use rfc.uri)
 (require-extension
-  (srfi 13))
+  (srfi 1 13))
 (use kirjasto.verkko) ;define-page-handler
 
-(include "src/index.scm")
-(include "src/niconico.scm")
-(include "src/test.scm")
-(include "src/feedtest.scm")
+(require "./src/index.scm")
+(require "./src/niconico.scm")
+(require "./src/test.scm")
+(require "./src/feedtest.scm")
 
 (define document-root
   (build-path (home-directory) ".site"))
