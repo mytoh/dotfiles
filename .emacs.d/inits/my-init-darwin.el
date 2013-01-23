493a397094bc7c1f251f5837b9bbd06a00daa2d0
mytoh <mytoh@gmail.com>
2013-01-23 15:52:57 +0900

    emacs: fix darwin-toggle-fullscree

--- .emacs.d/inits/my-init-darwin.el
+++ .emacs.d/inits/my-init-darwin.el
@@ -3,7 +3,7 @@
   (let ((my-paths `("~/local/homebrew/bin")))
     (setq exec-path (append my-paths exec-path))
     (setenv "PATH" (mapconcat 'identity my-paths ":")))
-  (define-key global-map (kbd "S-s-return") darwin-toggle-fullscreen))
+  (define-key global-map (kbd "s-S-RET") darwin-toggle-fullscreen))
 
 (define darwin-toggle-fullscreen ()
   "Toggle full scree on darwin"
