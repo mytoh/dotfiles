31b18ead8e339d1d9a9b0e1fb4fca6046a2cd5c1
mytoh <mytoh@gmail.com>
2013-03-01 03:08:59 +0900

    update

--- .stumpwm.d/conf/options.lisp
+++ .stumpwm.d/conf/options.lisp
@@ -6,8 +6,8 @@
   (set-fg-color "orange")
   (set-border-color "grey10")
   (set-msg-border-width 1)
-  ;; (set-font "-mplus-goth_p-medium-r-normal-*-10-*-*-*-*-*-iso10646-*")
-  (set-font  "-artwiz-lime-medium-r-normal--10-*-75-75-m-50-iso10646-1")
+  (set-font "-mplus-goth_p-medium-r-normal--10-*-75-75-p-50-iso8859-1")
+  ;; (set-font  "-artwiz-lime-normal-normal-normal-*-10-*-*-*-m-50-iso10646-1")
   ;;(set-font "-artwiz-limemod-medium-r-normal--10-110-75-75-m-50-iso8859-1")
   ;; windows
   (set-focus-color "khaki")
@@ -20,25 +20,25 @@
   ;; options
   (setf
    *startup-message*                 nil
-                                        ; border
+   ;; border
    *normal-border-width*             1
    *maxsize-border-width*            1
    *transient-border-width*          1
    *window-border-style*             :tight ;; thick,thin,none,tight
-                                        ; number map
+   ;; number map
    *frame-number-map*                "aoeuidhtnspyfgcrlqjkxbmwvz"
    *window-number-map*               "aoeuidhtns"
    *group-number-map*                "aoeuidhtns"
-                                        ; focus
+   ;; focus
    *mouse-focus-policy*              :sloppy
    *run-or-raise-all-groups*         t
    *new-window-preferred-frame*      '(:empty :focused)
-                                        ; gravity
+   ;; gravity
    *message-window-gravity*          :bottom-left
    *input-window-gravity*            :bottom-left
    *input-history-ignore-duplicates* t
 
-                                        ; formats
+   ;; formats
    *window-format*                   "^7*(^9*%n%m%15t^7*)^n" ;;^5 magenta
    *group-format*                    "^7*%t%s^n" ;;^7 white
    ))
