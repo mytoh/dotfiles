a2cbbd0d8f11dc2e67b7e1b0e8b30bb3054a34df
mytoh <mytoh@gmail.com>
2013-10-01 11:23:14 +0900

    add xat script, extattr wrapper for freebsd

--- .bin/ports-rebuild-all.sh
+++ .bin/ports-rebuild-all.sh
@@ -3,23 +3,23 @@
 #set -o nounset
 
 rebuild_all() {
-	local dir="/usr/ports/${1}"
-        local depends="$(make -C ${dir} all-depends-list)"
-        for p in ${depends}
-	do
-	sudo make -C ${p} reinstall clean
-	done
+    local dir="/usr/ports/${1}"
+    local depends="$(make -C ${dir} all-depends-list)"
+    for p in ${depends}
+    do
+        sudo make -s -C ${p} reinstall clean
+    done
 }
 
 rebuild_one() {
-local dir="/usr/ports/${1}"
+    local dir="/usr/ports/${1}"
 
-sudo make -C ${dir} clean reinstall clean
+    sudo make -s -C ${dir} clean reinstall clean
 }
 
 main() {
-rebuild_all ${1}
-rebuild_one ${1}
+    rebuild_all ${1}
+    rebuild_one ${1}
 }
 
 main "${1}"
--- .bin/ports-rebuild-run.sh
+++ .bin/ports-rebuild-run.sh
@@ -3,25 +3,24 @@
 set -o nounset
 
 rebuild() {
-	local dir="/usr/ports/${1}"
-        local depends="$(make -C ${dir} run-depends-list)"
-        for p in ${depends}
-	do
-	sudo make -C ${p} clean reinstall clean
-	done
-	
+    local dir="/usr/ports/${1}"
+    local depends="$(make -C ${dir} run-depends-list)"
+    for p in ${depends}
+    do
+        sudo make -s -C ${p} clean reinstall clean
+    done
 }
 
 rebuild_one() {
-local dir="/usr/ports/${1}"
+    local dir="/usr/ports/${1}"
 
-sudo make -C ${dir} clean reinstall clean
+    sudo make -s -C ${dir} clean reinstall clean
 }
 
 
 main() {
-rebuild ${1}
-rebuild_one ${1}
+    rebuild ${1}
+    rebuild_one ${1}
 }
 
 main "${1}"
