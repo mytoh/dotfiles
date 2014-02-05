9c1a6d0e3dc56b8ca3468e751aafec131b0bb2cd
mytoh <mytoh@gmail.com>
2014-02-06 03:17:23 +0900

    * .navi2ch/init.el: add navi2ch init file

---
+++ .navi2ch/init.el
@@ -0,0 +1,11 @@
+;;; init.el -*- lexical-binding: t -*-
+
+(setq navi2ch-mona-enable t)
+(setq navi2ch-mona-face-variable 'navi2ch-mona12-face)
+(setq navi2ch-ack-when-exit -1)
+(setq navi2ch-message-user-name "")
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
