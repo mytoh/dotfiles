b18f52f309f7b8b7b1a46af6979989269fa48db5
mytoh <mytoh@gmail.com>
2016-03-12 14:40:17 +0900

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,7 @@ set fish_greeting "><>"
 set -gx shellar ~/.config/shellar
 ## plugins
 set -gx shellar_bottles mytoh peco freebsd loitsu lehti nopea talikko emacs napa pikkukivi brew lol radio \
-        qjail 
+        qjail hoarder
 ## theme
 set -gx shellar_theme default
 ## custom
