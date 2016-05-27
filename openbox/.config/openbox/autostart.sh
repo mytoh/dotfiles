e4a7ad9e66550d6e6ecf825eb97f9ba4ffe43dcd
mytoh <mytoh@gmail.com>
2016-05-27 18:35:01 +0900

    add openbox

---
+++ openbox/.config/openbox/autostart.sh
@@ -0,0 +1,14 @@
+
+tint2 &
+
+if test -d ~/huone/kuvat/sivusto/4chan/a
+then
+    feh --randomize --bg-fill --recursive ~/huone/kuvat/sivusto/4chan/a &
+else
+    if test -f ~/.fehbg
+    then
+
+        sh ~/.fehbg &
+    fi
+
+fi
